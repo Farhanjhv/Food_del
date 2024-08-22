f9aa66ce83c0a326f234016294e75829e98e1c3a
Farhanjhv <124551600+Farhanjhv@users.noreply.github.com>
2024-08-22 21:17:02 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup'
 import Verify from './pages/Verify/Verify'
 import MyOrders from './pages/MyOrders/MyOrders'
+import Placeorder from './pages/PlaceOrder/Placeorder'
 
 
 const App = () => {
